29678f461d1bec07a7f654f7f46cef10da745a48
Sh0velMan <shovelman@gmail.com>
2016-10-18 11:44:10 -0500

    0.2 Release
    Many refinements, code updated to reflect new reference architecture
    (using an Arduino UNO R3).
    
    Updated Readme and added more comments to the code.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# arduino_3d_print_bed_heater_controller
-3D Print Bed Heater Controller for Arduino IDE
+# 3D Print Bed Heater Controller for Arduino IDE
+This sketch is a fully featured 3D Printer print bed heater controller for the Arduino IDE utilizing Adafruit's BMP280 breakout board for sensing temperature and the 7-seg LED Backpack and associated Adafruit libraries.
+
+This could also be used to control any other system which requires temperature feedback to turn a high-current (relatively speaking) device on and off, with simple modifications to the logic loop.
+
+*The minimum required hardware:*
+1. Arduino-compatible microcontroller with:
+  a. I2C Capability (for sensor and display).
+  b. 2 x GPIO Pins for use with momentary switches.
+  c. 1 x PWN-capable pin to control power output to the heating device.
+2. A logic-level N-channel MOSFET capable of handling the current requirements of your heating device.
+3. 2 x Momentary switches (buttons)
+  * These will trigger increment/decrement of target temperature when the GPIO pins are pulled high.
+4. Adafruit BMP280 Breakout Board (or similar)
+  * https://www.adafruit.com/product/2651
+  * https://www.sparkfun.com/products/13676 _Not tested, but I don't see why it wouldn't work, barring an address conflict or other issue I can't predict._
+  * Highly recommended to coat or cover the "business side" of the board with a thermally conductive but electrically non-conductive material which will decrease the likelihood of electrical short, physical damage to the sensor as well as provide some smoothing on the sensor response. These sensors react _extremely_ quickly to changes in temperature caused by basically anything. The slight puff of 'wind' created by moving your hand past the sensor will show a measureable drop in temperature for a second or two, for example. A coating on the components in question will lessen that effect greatly. Take care to use only enough  of whatever material you choose to do the job at hand. Using too much will introduce crippling sensor lag and compromise the entire system.
+5. Adafruit LED Backpack 7-segment display
+  * Dealer's choice on color. Here's my favorites:
+    * Green: https://www.adafruit.com/product/880
+    * White: https://www.adafruit.com/product/1002
+    * Blue: https://www.adafruit.com/product/881
+    * Yellow: https://www.adafruit.com/product/879
+6. A heating device.
+  * This can be a resistive element, thermoelectric cooler (flip it over or reverse polarity and it's a heater).
+7. A power supply.
+  * Many setups use 12 or 24 VDC power supplies. What you need depends entirely upon your heating device. Take care to design around a power supply voltage which your power MOSFET can tolerate (Drain-Source voltage, or Vds). While you are not required to _power_ your microcontroller from the same source, a common ground _is required_ in order for this design to function.
+
+*Extras:*
+* LEDs for indicators
+* Enclosure(s) for various components
+
+>Copyright 2016 Matt Gustafson (sh0velman)
+>
+>Licensed under the Apache License, Version 2.0 (the "License");
+>you may not use this file except in compliance with the License.
+>You may obtain a copy of the License at
+>
+>  http://www.apache.org/licenses/LICENSE-2.0
+>
+>Unless required by applicable law or agreed to in writing, software
+>distributed under the License is distributed on an "AS IS" BASIS,
+>WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+>See the License for the specific language governing permissions and
+>limitations under the License.
+>
+>All libraries utilized within this work are property of their respective
+>copyright owners are subject to the license terms outlined therein.
--- arduino_3d_print_bed_heater_controller.ino
+++ arduino_3d_print_bed_heater_controller.ino
@@ -1,12 +1,35 @@
-/***************************************************************************
-Name: 3D Print Bed Heater Controller for Arduino IDE
-Date: 10/12/2016
-Author: Matt Gustafson (sh0velman)
-Purpose: Controls the operation of a heated print bed for a 3D printer. This
+/**********************************************
+   Name: 3D Print Bed Heater Controller for Arduino IDE
+   Version: 0.2
+   Date: 10/18/2016
+   Author: Matt Gustafson (sh0velman)
+
+   Purpose: Controls the operation of a heated print bed for a 3D printer. This
         is important for high quality prints and safety when using a
         non-integrated bed heater that isn't controlled actively by the
         printer itself. See github for more.
-Platforms Tested: Adafruit Feather Huzzah ESP8266
+
+   Platforms Tested: Adafruit Feather Huzzah ESP8266, Arduino UNO R3
+
+   License:
+
+       Copyright 2016 Matt Gustafson (sh0velman)
+
+       Licensed under the Apache License, Version 2.0 (the "License");
+       you may not use this file except in compliance with the License.
+       You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing, software
+       distributed under the License is distributed on an "AS IS" BASIS,
+       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+       See the License for the specific language governing permissions and
+       limitations under the License.
+
+       All libraries utilized within this work are property of their respective
+       copyright owners are subject to the license terms outlined therein.
+
  ***************************************************************************/
 
 #include <Wire.h>
@@ -16,12 +39,15 @@ Platforms Tested: Adafruit Feather Huzzah ESP8266
 #include <Adafruit_LEDBackpack.h> // Library for Adafruit's 'LED Backpacks' which let you run many LEDs (or a 7 segment display as in this case) with just the I2C bus.
 #include <Adafruit_GFX.h> // A dependency of the LED Backpack library, not explicitly used in our case.
 
-int ctl = 12; // 'Control' pin, the pin which will control the flow of energy through our target device.
-int swtu = 13;  // Bump our target temperature up 1 degree per loop (up to 4 degrees per second in this case)
-int swtd = 14;   // Bump our target temperature down 1 degree per loop.
-int pwmmax = PWMRANGE; // Uncomment the version of this directly below to set an arbitrary 'ceiling' on the PWM signal we produce.
-// int pwmmax = 128;
+
+int ctl = 10; // 'Control' pin, the pin which will control the flow of energy through our target device. -- Arduino UNO config!
+int swtu = 11;  // Bump our target temperature up 1 degree per loop (up to 4 degrees per second in this case) -- Arduino UNO config!
+int swtd = 12;   // Bump our target temperature down 1 degree per loop. -- Arduino UNO config!
+int tmpPin = 1;
+//int pwmmax = PWMRANGE; // Uncomment the version of this directly below to set an arbitrary 'ceiling' on the PWM signal we produce. -- apparently not all boards have this defined by default, ex: Uno.
+int pwmmax = 255; // defining the 8 bit max here because the Uno doesn't have PWMRANGE defined by default.
 int pwmtarget;
+int loopcount;
 float temp;  // Variable to hold the last sampled bed temperature
 float targettemp; // Variable to hold the current target temperature
 float maxtemp = 115.00; // The maximum temperature (Celcius) we will allow the user to set via the buttons
@@ -30,108 +56,171 @@ float defaulttargettemp = 10.00; // Our default target temperature. Set low on p
 boolean adjustmode; // We'll set this 'true' when the user adjusts the temperature so that we can show the setting chosen on our display.
 unsigned long adjusttime; // The point in time when the user changed the temperature so that we can hide the setting chosen after a few seconds (adjustdelay)
 int adjustdelay = 4000; // 4 second delay before we go back to showing the current temperature of the bed after changing the targettemp
-Adafruit_BMP280 bmp; // variable to hold the instance of our sensor, configured by default to use I2C for communication.
+Adafruit_BMP280 sensor; // variable to hold the instance of our sensor, configured by default to use I2C for communication.
 Adafruit_7segment disp = Adafruit_7segment(); // variable to hold the instance of our 7 segment display
 
 void setup() {
-  disp.begin(0x70);
-  Serial.begin(9600);
-  Serial.println(F("."));
-  Serial.println(F(".."));
-  Serial.println(F("..."));
-  Serial.println(F("...."));
-  Serial.println(F("....."));
-  Serial.println(F("......"));
-  Serial.println(F("Starting up... Letting system settle..."));
-  if (!bmp.begin()) {
-    Serial.println(F("FATAL ERROR: Could not find a valid BMP280 sensor, check wiring!"));
-    Serial.println(F("Halting!"));
-    displayError();
-    delay(2000);
-    while (1);
-  }
+        disp.begin(0x70); // display is on I2C address 0x70 in default config, can be changed by soldering jumpers.
+        Serial.begin(9600);
+        Serial.println(F("Starting up... Letting system settle..."));
 
-  disp.print(88.88);
-  disp.writeDisplay();
+        if (!sensor.begin()) {
+                Serial.println(F("FATAL ERROR: Could not find a valid BMP280 sensor, check wiring!"));
+                Serial.println(F("Halting!"));
+                displayError();
+                delay(2000);
+                while (1) ;
+        }
 
-  targettemp = defaulttargettemp; // default target temp
-  pinMode (ctl, OUTPUT);  // make pin 'ctl' an output
-  adjustmode = false;
-  adjusttime = millis();
-  delay(4000);
-  Serial.println(F("Completed... Running now!"));
+
+
+// this code just displays a fun animation on the display during startup. Can be removed if you're the dour sort.
+        disp.writeDigitRaw(0,B00000001);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(1,B00000001);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(1,B00000011);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(3,B00010000);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(3,B00011000);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(4,B00001000);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(4,B00001100);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(4,B00001110);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(4,B00001111);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(3,B00011001);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(3,B00111001);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(1,B00000111);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(1,B00001111);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(0,B00001001);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(0,B00011001);
+        disp.writeDisplay();
+        delay(150);
+        disp.writeDigitRaw(0,B00111001);
+        disp.writeDisplay();
+        delay(250);
+        disp.print(8888);
+        disp.writeDisplay();
+// end our silly antics
+
+        targettemp = defaulttargettemp; // default target temp
+        pinMode (ctl, OUTPUT); // make pin 'ctl' an output
+        adjustmode = false;
+        adjusttime = millis();
+        delay(2000);
+        disp.clear();
+        disp.setBrightness(7);
+        disp.writeDisplay();
+        loopcount = 0;
+        Serial.println(F("Completed... Running now!"));
 }
 
 void loop() {
-  temp = bmp.readTemperature();
-  if (adjustmode = true)
-  {
-      if (millis() > (adjusttime + adjustdelay))
+        temp = sensor.readTemperature();
+        if (adjustmode == true)
         {
-          adjustmode = false;
-          disp.print(temp);
-          disp.writeDisplay();
+                if (millis() > (adjusttime + adjustdelay))
+                {
+                        adjustmode = false;
+                        disp.setBrightness(7);
+                        disp.print(temp);
+                        disp.writeDisplay();
+
+                        loopcount = 0;
+                } else {
+                        disp.setBrightness(15);
+                        disp.print(targettemp);
+                        disp.writeDisplay();
+                }
         } else {
-          disp.print(targettemp);
-          disp.writeDisplay();
-          }
-  } else {
-          disp.print(temp);
-          disp.writeDisplay();
-  }
-    if (digitalRead(swtu)== 1 ) // user is pressing the UP button
-    {
-        adjusttime = millis(); // set our time of adjustment
-        adjustmode = true; // set the adjustment mode to true so we can show the new setting on the display
-     if (targettemp < maxtemp) // if we aren't already at the max temp
-      {
-        targettemp += 1.0; // increment the value by 1.0
-      } else { // if we're already at the max temp, set it to the max again to be safe and move on
-        targettemp = maxtemp;
-      }
-    } else  if (digitalRead(swtd)== 1 ) // user is pressing the DOWN button
-    {
-        adjusttime = millis();
-        adjustmode = true;
-     if (targettemp > mintemp) // if we aren't already at the minimum temp
-      {
-        targettemp -= 1.0; // decrement the value by 1.0
-      } else { // if we're already at the minimum, set it to the minimum again to be safe and move on
-        targettemp = mintemp;
-      }
-    }
-  if (temp < 0) // To catch a failed sensor and prevent the heater from coming on full blast indefinitely, we check if the value is less than 0. If you are trying to 3D print in a sub-(water)freezing space, this code would have to be adjusted.
-  {
-    Serial.print(F("ERROR: Measured Temp = "));
-    Serial.print(temp);
-    Serial.println(" *C");
-    Serial.println(F("Failed sensor?"));
-    displayError();
-    pwmtarget = 0; // We will hold the pwmtarget at zero until we get a valid (valid-looking anyway) temperature reading to work off of.
-  } else if (temp >= targettemp) // Measured temperature is at or above target, turn off the output.
-    {
-      pwmtarget = 0;
-    } else if (temp > (targettemp - 1.0))// Measured temperature is close to target, cut the power back to 25% to try and level in without overshoot.
-    {
-      pwmtarget = pwmmax * 0.25;
-    } else if (temp > (targettemp - 2.0))// Measured temperature is closing to target, cut the power back to 50%.
-    {
-      pwmtarget = pwmmax * 0.5;
-    }  else if (temp > (targettemp - 5.0))// Measured temperature is within 5 degrees of target, cut the power back to 75% to let any gradients that have built up start to equalize.
-    {
-      pwmtarget = pwmmax * 0.75;
-    } else { // Measured temperature is more than 5 degrees below target, run the power at max.
-      pwmtarget = pwmmax;
-    }
-  analogWrite(ctl, pwmtarget); // Write the PWM target to the pin.
-  delay (250);
+
+
+                if (loopcount >= 2)
+                {
+                        disp.print(temp);
+                        disp.writeDisplay();
+                        loopcount = 0;
+                } else {
+                        loopcount+=1;
+                }
+        }
+        if (digitalRead(swtu)== 1 ) // user is pressing the UP button
+        {
+                adjusttime = millis(); // set our time of adjustment
+                adjustmode = true; // set the adjustment mode to true so we can show the new setting on the display
+                if (targettemp < maxtemp) // if we aren't already at the max temp
+                {
+                        targettemp += 1.0; // increment the value by 1.0
+                } else { // if we're already at the max temp, set it to the max again to be safe and move on
+                        targettemp = maxtemp;
+                }
+        } else if (digitalRead(swtd)== 1 ) // user is pressing the DOWN button
+        {
+                adjusttime = millis();
+                adjustmode = true;
+                if (targettemp > mintemp) // if we aren't already at the minimum temp
+                {
+                        targettemp -= 1.0; // decrement the value by 1.0
+                } else { // if we're already at the minimum, set it to the minimum again to be safe and move on
+                        targettemp = mintemp;
+                }
+        }
+        if (temp < 0) // To catch a failed sensor and prevent the heater from coming on full blast indefinitely, we check if the value is less than 0. If you are trying to 3D print in a sub-(water)freezing space, this code would have to be adjusted.
+        {
+                Serial.print(F("ERROR: Measured Temp = "));
+                Serial.print(temp);
+                Serial.println(" *C");
+                Serial.println(F("Failed sensor?"));
+                displayError();
+                pwmtarget = 0; // We will hold the pwmtarget at zero until we get a valid (valid-looking anyway) temperature reading to work off of.
+        } else if (temp >= targettemp) // Measured temperature is at or above target, turn off the output.
+        {
+                pwmtarget = 0;
+        } else if (temp > (targettemp - 0.5))// Measured temperature is close to target, cut the power back to 40% to try and level in without overshoot.
+        {
+                pwmtarget = pwmmax * 0.4;
+        } else if (temp > (targettemp - 1.0))// Measured temperature is closing to target, cut the power back to 75%.
+        {
+                pwmtarget = pwmmax * 0.75;
+        }  else if (temp > (targettemp - 5.0))// Measured temperature is within 5 degrees of target, cut the power back to 75% to let any gradients that have built up start to equalize.
+        {
+                pwmtarget = pwmmax * 1; // just leaving this here in case another level of adjustment is needed in the future.
+        } else { // Measured temperature is more than 5 degrees below target, run the power at max.
+                pwmtarget = pwmmax;
+        }
+        analogWrite(ctl, pwmtarget); // Write the PWM target to the pin.
+        delay(500);
 }
 
 void displayError() // function to show 'Err' on the display if something goes wrong. Can only be called after the disp.begin() in the setup block.
 {
-  disp.writeDigitRaw(0,121); // "E"
-  disp.writeDigitRaw(1,80); // "r"
-  disp.writeDigitRaw(3,80); // "r"
-  disp.writeDigitRaw(4,0); // " "
-  disp.writeDisplay(); // Write it out to the display.
+        disp.writeDigitRaw(0,121); // "E"
+        disp.writeDigitRaw(1,80); // "r"
+        disp.writeDigitRaw(3,80); // "r"
+        disp.writeDigitRaw(4,0); // " "
+        disp.writeDisplay(); // Write it out to the display.
 }
